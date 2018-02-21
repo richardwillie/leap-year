faff9c4cb74bb4e97c5fd069f1995ba10150d66d
sammwaura <meshsami27@gmail.com>
2018-02-21 10:16:24 +0300

    add fourth spec

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
 });
 
 var leapYear = function (year) {
-    if (year % 100 === 0) {
-      return false;
-  } else if (year % 4 === 0) {
-    return true;
+    if ((year % 4 === 0) && (year % 100 !=== 0) {
+      return true;
   } else {
     return false;
   }
 };
+
+
 // jQuery user interface logic here.
